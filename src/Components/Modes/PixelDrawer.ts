d05816cc87122d073737aa3342e902dafc5e6ba0
Joshua Cooper <jc1520@ic.ac.uk>
2023-11-08 22:51:09 +0000

    full pixel drawing test

--- src/Components/Modes/PixelDrawer.ts
+++ src/Components/Modes/PixelDrawer.ts
@@ -90,7 +90,7 @@ export class PixelDrawer extends BaseMode {
 					toSend.writeUInt8(green, adjPixelNum * 3 + 1);
 					toSend.writeUInt8(blue, adjPixelNum * 3 + 2);
 
-					const mirPixelNum = 32 + (31 - c) + r * 2 * cols;
+					const mirPixelNum = 64 + (63 - c) + r * 2 * cols;
 					toSend.writeUInt8(red, mirPixelNum * 3);
 					toSend.writeUInt8(green, mirPixelNum * 3 + 1);
 					toSend.writeUInt8(blue, mirPixelNum * 3 + 2);
